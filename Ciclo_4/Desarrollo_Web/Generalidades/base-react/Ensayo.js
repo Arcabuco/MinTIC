a0ffa9deb49755599ac7c730ceea0dc597c8ece7
Mauricio S. Herrera Palacio <mauricioherrerapalacio@gmail.com>
2021-11-05 18:41:19 -0500

    Bases de Node y React

--- Ciclo_4/Desarrollo_Web/Generalidades/base-react/Ensayo.js
+++ Ciclo_4/Desarrollo_Web/Generalidades/base-react/Ensayo.js
@@ -1,8 +1,15 @@
 console.log("Holiwi")
 
-var a = 10
-a = a + 1
-console.log("a >", a)
+//var a = 10
+//a = a + 1
+//console.log("a >", a)
 
-var arr = [1,2,3,4,5,6,7,8,9,10]
-arr.map(item => console.log(item))
+//var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//arr.map(item => console.log(item))
+
+const fetch = require("node-fetch")
+var response = fetch("https://api.github.com/amontealegrer")
+    .then(response => response.json())
+    .then(response => console.log(response))
+
+
